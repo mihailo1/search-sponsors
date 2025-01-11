425486b08945b77a3b8ae766b38487da504dbb62
Misha Nikiforov <mikhail@joinsmiler.com>
2025-01-11 11:38:54 +0100

    2025-update-2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from 'globals';
-import * as tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import * as pluginJs from '@eslint/js';
+import * as tsPlugin from '@typescript-eslint/eslint-plugin';
 import * as pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
@@ -9,21 +9,24 @@ export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
-      parser: tsParser, // Use TypeScript parser
+      parser: tsParser, // TypeScript parser
       globals: globals.browser,
     },
-    ...pluginJs.configs.recommended, // Include the recommended config from @eslint/js
+    rules: {
+      ...pluginJs.configs.recommended.rules, // JavaScript recommended rules
+    },
   },
   {
+    files: ['**/*.ts', '**/*.tsx'],
     plugins: {
       '@typescript-eslint': tsPlugin,
     },
     rules: {
-      ...tsPlugin.configs.recommended.rules, // Include TypeScript recommended rules
-      ...(tsPlugin.configs['recommended-type-checked']?.rules || {}), // Include type-checked rules if available
+      ...tsPlugin.configs.recommended.rules, // TypeScript recommended rules
     },
   },
   {
+    files: ['**/*.jsx', '**/*.tsx'],
     plugins: {
       react: pluginReact,
     },
@@ -33,7 +36,7 @@ export default [
       },
     },
     rules: {
-      ...pluginReact.configs.recommended.rules, // Include React recommended rules
+      ...pluginReact.configs.recommended.rules, // React recommended rules
     },
   },
 ];
