50507af01d257269b98f9184a1a34d62f6c0a0fb
Misha Nikiforov <mikhail@joinsmiler.com>
2025-01-11 02:58:54 +0100

    2025-update-2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,11 @@ import pluginJs from '@eslint/js';
 import * as tseslint from '@typescript-eslint/eslint-plugin';
 import pluginReact from 'eslint-plugin-react';
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...(tseslint.configs.recommended ? [tseslint.configs.recommended] : []),
-  pluginReact.configs.flat.recommended,
+  ...pluginJs.configs.recommended,
+  ...(tseslint.configs.recommended ? [...tseslint.configs.recommended] : []),
+  ...pluginReact.configs.flat.recommended,
 ];
